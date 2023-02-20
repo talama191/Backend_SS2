f844d979203f55334ab938ca2a7c1699e0d892e8
LAPTOP-50N9021N\Admin <talama191@gmail.com>
2023-02-20 20:15:35 +0700

    Setup Spring project

---
+++ SS2Spring/src/main/java/com/example/ss2spring/Ss2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ss2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2SpringApplication.class, args);
+    }
+
+}
