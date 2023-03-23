da82d12fba4be0565e560e17828b9974b3cdfa26
son.ndc <sn.coffeelake@gmail.com>
2023-03-24 04:29:48 +0700

    - Fix: + Enable CORS

--- EcommerceBackend/src/main/java/com/example/ecommercebackend/User/UserController.java
+++ EcommerceBackend/src/main/java/com/example/ecommercebackend/User/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
     @Autowired
     UserService userService;
