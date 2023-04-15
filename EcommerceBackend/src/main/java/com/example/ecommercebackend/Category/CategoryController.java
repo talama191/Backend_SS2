0202b456571c67c0fb12e2d1d3b4a009aea4332c
son.ndc <sn.coffeelake@gmail.com>
2023-04-15 14:43:59 +0700

    - Imple: + Get all category

--- EcommerceBackend/src/main/java/com/example/ecommercebackend/Category/CategoryController.java
+++ EcommerceBackend/src/main/java/com/example/ecommercebackend/Category/CategoryController.java
@@ -3,9 +3,7 @@ package com.example.ecommercebackend.Category;
 import com.example.ecommercebackend.Response.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -17,4 +15,5 @@ public class CategoryController {
     public ResponseData getAllCategory() {
         return new ResponseData(categoryService.getAllCategory(),200, HttpStatus.OK);
     }
+
 }
--- EcommerceBackend/src/main/java/com/example/ecommercebackend/Category/CategoryService.java
+++ EcommerceBackend/src/main/java/com/example/ecommercebackend/Category/CategoryService.java
@@ -13,4 +13,5 @@ public class CategoryService {
     public List<Category> getAllCategory() {
         return categoryRepository.findAll();
     }
+
 }
